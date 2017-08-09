0c0cb27b2a13e467d80ba6a33333d659f1e7f741
Simon Anger <simonanger89@gmail.com>
2017-08-09 11:16:00 +0100

    add dice roll

--- dice.rb
+++ dice.rb
@@ -1,14 +1,28 @@
 class Dice
 
-  attr_reader(:dice_name, :sides)
-  attr_writer(:dice_name, :sides)
+  attr_reader(:dice_name, :dice_sides)
+  attr_writer(:dice_name, :dice_sides)
 
-  def initialize(dice_name, sides)
+  def initialize(dice_name, dice_sides)
     @dice_name = dice_name
-    @sides = sides
+    @dice_sides = dice_sides
   end
 
+  def name
 
+    return  @dice_name
+  end
+
+def sides
+
+  return @dice_sides
+
+end
+
+def roll
+  return rand(1..(@dice_sides))
+
+end
 
 
 
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,14 +3,27 @@ require_relative( "../dice.rb" )
 
 class TestDice < MiniTest::Test
 
-  def test_dice_name
-
-    dice = Dice.new ( "Dice 1", 6 )
-
-    assert_equal( "Dice 1" , dice.name )
+  def setup()
+    @dice = Dice.new( "Dice 1", 6 )
   end
 
+  def test_dice_name
 
+    assert_equal( "Dice 1" , @dice.name )
+  end
+
+  def test_dice_sides
+    assert_equal( 6, @dice.sides)
+  end
+
+# We tested the random number generator between 1-6 using puts but could not
+# write a test that would result in the same 2 numbers being generated
+# (as random)
+# def test_dice_roll
+#
+#   # puts @dice.roll()
+#
+# end
 
 
 
